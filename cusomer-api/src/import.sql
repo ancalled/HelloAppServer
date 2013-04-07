42812427d1c51e65b9465641aeabcf10378ffec6
Andrey Koltochnik <ankolt@gmail.com>
2013-04-07 11:47:13 +0600

    build.xml

--- cusomer-api/src/import.sql
+++ cusomer-api/src/import.sql
@@ -1,27 +1,27 @@
 
-INSERT INTO cities(name, title) VALUES ('almaty', 'Алматы');
-INSERT INTO cities(name, title) VALUES ('astana', 'Астана');
-
-INSERT INTO categories(title, name) VALUES ('Бары', 'bars-pubs');
-INSERT INTO categories(title, name) VALUES ('Кафе', 'cafes');
-INSERT INTO categories(title, name) VALUES ('Кинотеатры', 'cinemas');
-INSERT INTO categories(title, name) VALUES ('Кофейни', 'coffee-houses');
-
-INSERT INTO places(name, title, category, city) VALUES ('chukotka', 'Чукотка', 'bars-pubs', 'almaty');
-INSERT INTO places(name, title, category, city) VALUES ('totbar', 'Тот самый бар', 'bars-pubs', 'almaty');
-INSERT INTO places(name, title, category, city) VALUES ('nebar', 'Nebar', 'bars-pubs', 'almaty');
-INSERT INTO places(name, title, category, city) VALUES ('mojo', 'Mojo', 'bars-pubs', 'almaty');
-INSERT INTO places(name, title, category, city) VALUES ('fantomas', 'Синий Fun To Mass', 'bars-pubs', 'almaty');
-INSERT INTO places(name, title, category, city) VALUES ('zhest', 'Жесть', 'bars-pubs', 'almaty');
-INSERT INTO places(name, title, category, city) VALUES ('cafeteria', 'Cafeteria', 'coffee-houses', 'almaty');
-INSERT INTO places(name, title, category, city) VALUES ('shokoladnica', 'Шоколадница', 'coffee-houses', 'almaty');
-INSERT INTO places(name, title, category, city) VALUES ('cofedelia', 'Coffeedelia', 'coffee-houses', 'almaty');
-INSERT INTO places(name, title, category, city) VALUES ('madlen', 'Мадлен', 'coffee-houses', 'almaty');
-# INSERT INTO places(name, title, category, city) VALUES ('', '', 'coffee-houses', 'almaty');
-
-
-INSERT INTO events(id, title, place, eventDate, description) VALUES (1, 'Концерт Дельфина', 'zhest', '2013-03-22 21:00:00', ' На концерте мы услышим известные боевики, а также песни из нового альбома «Существо». Начало концерта в 23:00. Цена билета 5000 тг. и 8000 тг. (балкон)');
-
+# INSERT INTO cities(name, title) VALUES ('almaty', 'Алматы');
+# INSERT INTO cities(name, title) VALUES ('astana', 'Астана');
+#
+# INSERT INTO categories(title, name) VALUES ('Бары', 'bars-pubs');
+# INSERT INTO categories(title, name) VALUES ('Кафе', 'cafes');
+# INSERT INTO categories(title, name) VALUES ('Кинотеатры', 'cinemas');
+# INSERT INTO categories(title, name) VALUES ('Кофейни', 'coffee-houses');
+#
+# INSERT INTO places(name, title, category, city) VALUES ('chukotka', 'Чукотка', 'bars-pubs', 'almaty');
+# INSERT INTO places(name, title, category, city) VALUES ('totbar', 'Тот самый бар', 'bars-pubs', 'almaty');
+# INSERT INTO places(name, title, category, city) VALUES ('nebar', 'Nebar', 'bars-pubs', 'almaty');
+# INSERT INTO places(name, title, category, city) VALUES ('mojo', 'Mojo', 'bars-pubs', 'almaty');
+# INSERT INTO places(name, title, category, city) VALUES ('fantomas', 'Синий Fun To Mass', 'bars-pubs', 'almaty');
+# INSERT INTO places(name, title, category, city) VALUES ('zhest', 'Жесть', 'bars-pubs', 'almaty');
+# INSERT INTO places(name, title, category, city) VALUES ('cafeteria', 'Cafeteria', 'coffee-houses', 'almaty');
+# INSERT INTO places(name, title, category, city) VALUES ('shokoladnica', 'Шоколадница', 'coffee-houses', 'almaty');
+# INSERT INTO places(name, title, category, city) VALUES ('cofedelia', 'Coffeedelia', 'coffee-houses', 'almaty');
+# INSERT INTO places(name, title, category, city) VALUES ('madlen', 'Мадлен', 'coffee-houses', 'almaty');
+# # INSERT INTO places(name, title, category, city) VALUES ('', '', 'coffee-houses', 'almaty');
+#
+#
+# INSERT INTO events(id, title, place, eventDate, description) VALUES (1, 'Концерт Дельфина', 'zhest', '2013-03-22 21:00:00', ' На концерте мы услышим известные боевики, а также песни из нового альбома «Существо». Начало концерта в 23:00. Цена билета 5000 тг. и 8000 тг. (балкон)');
+#
 
 # ---------------------------------------------------------------------------
 
